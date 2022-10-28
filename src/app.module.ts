1591b265e6b2f6ae6d9dde07bbeb81211c223d5d
Muhammad Haris Firmansyah <acc.cp.haris@gmail.com>
2022-10-28 19:10:57 +0700

    Add env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KaryawanModule } from './karyawan/karyawan.module';
 import { TugasModule } from './tugas/tugas.module';
+import appConfig from './configs/app.config';
+import databaseConfig from './configs/database.config';
 
 @Module({
-  imports: [KaryawanModule, TugasModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig, databaseConfig],
+      isGlobal: true,
+    }),
+    KaryawanModule,
+    TugasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/database.config.ts
@@ -0,0 +1,14 @@
+import { registerAs } from '@nestjs/config';
+import { Karyawan } from 'src/karyawan/entities/karyawan.entity';
+import { Tugas } from 'src/tugas/entities/tugas.entity';
+
+export default registerAs('database', () => ({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  type: process.env.DATABASE_TYPE,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  name: process.env.DATABASE_NAME,
+  synchronize: false,
+  entities: [Karyawan, Tugas],
+}));
